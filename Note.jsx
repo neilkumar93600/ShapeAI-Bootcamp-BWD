b5cb001093f833add6b0fc4ff0993df09be9f9a0
Nilesh kumar <71492461+neilkumar93600@users.noreply.github.com>
2021-06-26 17:53:43 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="note">
+      <h1>lorem</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+        asperiores incidunt ad dicta id tenetur.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
